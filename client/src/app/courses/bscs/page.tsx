4855f9946f81321f3cc8e3ac0228a533f35e1bca
Merriam Atun <merriamatun@gmail.com>
2025-01-26 22:36:11 +0800

    Added contact and courses

---
+++ client/src/app/courses/bscs/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function bscs() {
+  return (
+    <b>Bachelor of Science in Information System</b>
+  )
+}
+
+export default bscs
---
+++ client/src/app/courses/bsis/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function bsis() {
+  return (
+    <b>Bachelor of Science in Information System</b>
+  )
+}
+
+export default bsis
